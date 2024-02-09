670410a7cdcf3b46e56198a5bf9d779ac9c91afe
BlackSmith-6144 <155072240+BlackSmith-6144@users.noreply.github.com>
2024-02-09 16:31:46 +0600

    Create rpsg.py
    In this file I make a text based rock, paper and seizer game using python. And in this game the bot wins all times.

---
+++ rpsg.py
@@ -0,0 +1,11 @@
+print("            (A ROCK,PAPER,SEIZER TEXT BASED GAME WHERE THE BOT WIN ALL TIME)            ")
+mo1 = "Rock"
+mo2 = "Paper"
+mo3 = "Seizer"
+usermo = input("Your move:")
+if usermo == mo1:
+    print("Bot move: Paper")
+elif usermo == mo2:
+    print("Bot move: Seizer")
+elif usermo == mo3:
+    print("Bot move: Rock")
