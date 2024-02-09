0fdeab889767e0dc21105695a26a384da6ea6559
BlackSmith-6144 <155072240+BlackSmith-6144@users.noreply.github.com>
2024-02-09 16:14:20 +0600

    Create minisearch.py
    In this file I make a mini search engine using If,elif and else statements.

---
+++ minisearch.py
@@ -0,0 +1,15 @@
+poten = "Facebook"
+poten2 = "Youtube"
+poten3 = "Reddit"
+poten4 = "Quora"
+search = input("Mini web search:")
+if search == poten2:
+    print("Result: youtube.com\nA website where users can upload their videoes and can watch those videoes.")
+elif search == poten3:
+    print("Result: reddit.com\nA website that doesn't have very much censorship and people discuss and upload info in community.")
+elif search == poten:
+    print("Result: facebook.com\nJust a social media website where people can upload information with censorship and mostly uploaded indivisually.")
+elif search == poten4:
+    print("Result: quora.com\nA website where people ask questions and answer it.")
+else: 
+    print("Result: not founded")
